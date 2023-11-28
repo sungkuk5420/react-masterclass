2676c266ac8d7ce879c0d275a53f1be76c0b979f
Sungkuk Kim <sungkuk5420@gmail.com>
2023-11-28 17:47:20 +0900

    스타일 컴포넌트 확장

--- src/App.js
+++ src/App.js
@@ -5,27 +5,21 @@ const Father = styled.div`
 display:  flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const Text = styled.span`
-color: white;
+const Circle = styled(Box)`
+  border-radius: 50%;
 `;
+
 
 function App() {
   return <Father>
-    <BoxOne>
-      <Text>Hello</Text>
-    </BoxOne>
-    <BoxTwo/>
+    <Box bgColor="teal"/>
+    <Circle bgColor="tomato"/>
   </Father>
 }
 
