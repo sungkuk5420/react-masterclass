65d8512eb6ac0ea680527d7a8efbc3f227095005
Sungkuk Kim <sungkuk5420@gmail.com>
2023-11-29 10:46:37 +0900

    darkmode 설정하는법

--- src/App.js
+++ src/App.js
@@ -1,53 +1,23 @@
-import styled,{keyframes} from "styled-components";
+import styled from "styled-components";
 
+const Title = styled.h1`
+  color:${(props)=>props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
-display:  flex;
-`;
-
-const rotateAnimation = keyframes`
-  0%{
-    transform:  rotate(0deg);
-    border-radius: 0px;
-  }
-  50%{
-    transform:  rotate(360deg);
-    border-radius: 100px;
-  }
-  100%{
-    transform:  rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size:36px;
-  &:hover{
-    font-size:48px;
-  }
-`;
-
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: tomato;
-  animation:${rotateAnimation} 1s linear infinite;
-  display: flex;
-  align-items: center;
+  display:  flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
-  ${Emoji}{
-    font-size: 25px;
-    &:hover{
-      font-size: 98px;
-    }
-  }
-  `;
+  align-items: center;
+  background-color: ${(props)=>props.theme.backgroundColor};
+`;
+
 function App() {
   return <Wrapper as="header">
-    <Box>
-      <Emoji>😁</Emoji>
-    </Box>
-    <Emoji>😁</Emoji>
+    <Title>
+      Hello
+    </Title>
   </Wrapper>
 }
 
