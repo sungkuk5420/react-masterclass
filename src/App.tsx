21c92667b48871d54e3d42bf1a8ba07bfeb47022
Sungkuk Kim <sungkuk5420@gmail.com>
2023-11-29 15:34:02 +0900

    typescript form event

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import styled from "styled-components";
-import Circle from "./Circle";
-
+import {useState } from "react"
 
 function App() {
+  const [value,setValue] =useState("");
+  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+    console.log(event.currentTarget.value)
+    const {currentTarget:{value}} = event
+    setValue(value);
+  };
+
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello",value)
+  }
   return <div>
-    <Circle bgColor="teal" borderColor="red"></Circle>
-    <Circle bgColor="tomato" borderColor="blue" text="circle"></Circle>
+    <form onSubmit={onSubmit}>
+      <input onChange ={onChange} value={value} type="text" placeholder="username"></input>
+      <button>Log in</button>
+    </form>
   </div>
 }
 
