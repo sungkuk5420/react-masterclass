f8fb850849c04febc112eed0f28c6e43fa17f393
Sungkuk Kim <sungkuk5420@gmail.com>
2023-11-29 13:08:56 +0900

    옵셔널 프롭 설정방법

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import Circle from "./Circle";
 
 function App() {
   return <div>
-    <Circle bgColor="teal"></Circle>
-    <Circle bgColor="tomato"></Circle>
+    <Circle bgColor="teal" borderColor="red"></Circle>
+    <Circle bgColor="tomato" borderColor="blue" text="circle"></Circle>
   </div>
 }
 
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,19 +2,29 @@ import styled from "styled-components";
 
 interface ContainerProps {
   bgColor:string;
+  borderColor:string;
 }
 const Container = styled.div<ContainerProps>`
   background-color: ${(props)=>props.bgColor};
+  border: 1px solid ${(props)=>props.borderColor} ;
   width: 200px;
   height: 200px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 interface CircleProps {
   bgColor:string;
+  borderColor?:string;
+  text?:string;
 }
 
-export default function Circle({bgColor}: CircleProps){
-  return <Container bgColor={bgColor}></Container>
+export default function Circle({bgColor, borderColor,text= "default text"}: CircleProps){
+  return <Container bgColor={bgColor} borderColor = {borderColor??bgColor}>
+    {text}
+  </Container>
 }
 
 interface PlayerShape {
