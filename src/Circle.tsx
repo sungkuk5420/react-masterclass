ed4d42671913a96dd208f33ece8ce4202b1f170f
Sungkuk Kim <sungkuk5420@gmail.com>
2023-11-29 12:56:39 +0900

    인터페이스 정의 및 프롭 타입의 정의

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,7 +1,27 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
-
-export default function Circle(){
-  return <Container></Container>
+interface ContainerProps {
+  bgColor:string;
 }
+const Container = styled.div<ContainerProps>`
+  background-color: ${(props)=>props.bgColor};
+  width: 200px;
+  height: 200px;
+`;
+
+interface CircleProps {
+  bgColor:string;
+}
+
+export default function Circle({bgColor}: CircleProps){
+  return <Container bgColor={bgColor}></Container>
+}
+
+interface PlayerShape {
+  name:string;
+  age:number;
+}
+
+const sayHello = (playerObj:PlayerShape) => `Hello ${playerObj.name} you ar ${playerObj.age} years old.`;
+
+sayHello({name:"kim sungkuk",age:12});
